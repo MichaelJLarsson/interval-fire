21b104a3cba927f25e11964a0ae6bce8a5ffb7e1
Mikael Larsson <mikael.j.larsson@hotmail.com>
2026-09-04 10:07:17 +0200

    feat: add husky pre-commit hook and configure lint-staged for code formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config')
 const expoConfig = require('eslint-config-expo/flat')
+const prettierConfig = require('eslint-config-prettier')
 const simpleImportSort = require('eslint-plugin-simple-import-sort')
 
 module.exports = defineConfig([
   expoConfig,
+  prettierConfig,
   {
     ignores: ['dist/*'],
   },
